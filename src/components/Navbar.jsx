5f85f6b31938889ca2b639bdd9bc69dfd55fc7be
Jacquelinekudzi <jacquekudzi4@gmail.com>
2024-05-24 11:53:39 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             <div className="flex-1 hidden max-w-xs ml-40 mr-auto lg:block">
               <label htmlFor="" className="sr-only">
                 {" "}
-                Search{" "}
+                Search here{" "}
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
