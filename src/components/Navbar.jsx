81876ea8cee13252d6a72e3ed07e833bbe20b844
anesuraswe <ane.uraswe@gmail.com>
2024-05-24 13:00:14 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@
 
 import Header from "./Header";
 import { FaHeadphonesAlt } from "react-icons/fa";
+import { TbLogout } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-1 bg-white">
-      <div className="hidden md:flex md:w-64 md:flex-col">
-        <div className="flex flex-col flex-grow pt-5 overflow-y-auto bg-gray-100">
+    <div className="flex flex-1 h-screen bg-black">
+      <div className="">
+        <div className="flex flex-col h-screen pt-5  bg-gray-100">
           <div className="flex items-center flex-shrink-0 px-4">
             <img
               className="w-auto h-8"
@@ -59,30 +60,24 @@ const Navbar = () => {
                     <button className="text-center items-center justify-center w-full bg-white mt-2 py-1 px-6 rounded-md">Contact us</button>
 
                   </div>
+                
                 <a
                   href="#"
-                  className=" flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
+                  className="flex items-center px-4 py-2.5 text-lg font-semibold transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
                 >
-                  Settings
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
-                >
+                  <TbLogout className="mr-4 text-xl mt-1 font-semibold"/>
                   Log Out
                 </a>
                 </nav>
               </nav>
 
-              <nav className="space-y-2 mt-64">
-                
-              </nav>
+              
             </div>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 max-h-screen">
         <main>
           <div className="py-6">
             <div className="px-4  max-w-7xl sm:px-6 md:px-8">
