47b64e4046e50df37865e5dd7c3d1bef83db038b
anesuraswe <ane.uraswe@gmail.com>
2024-05-24 12:49:00 +0200

    Contact us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 // import React, { useState } from 'react';
 
 import Header from "./Header";
+import { FaHeadphonesAlt } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-1 bg-gray-50">
+    <div className="flex flex-1 bg-white">
       <div className="hidden md:flex md:w-64 md:flex-col">
-        <div className="flex flex-col flex-grow pt-5 overflow-y-auto bg-white">
+        <div className="flex flex-col flex-grow pt-5 overflow-y-auto bg-gray-100">
           <div className="flex items-center flex-shrink-0 px-4">
             <img
               className="w-auto h-8"
@@ -16,28 +17,14 @@ const Navbar = () => {
           </div>
 
           <div className="flex flex-col flex-1 px-3 mt-6">
-            <div className="space-y-4">
-              <nav className="flex-1 space-y-2">
+            <div className="space">
+              <nav className="flex-1">
                 <a
                   href="#"
                   title=""
                   className="flex items-center px-4 py-2.5 text-sm font-medium text-white transition-all duration-200 bg-indigo-600 rounded-lg group"
                 >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 mr-4 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
+                  
                   Dashboard
                 </a>
 
@@ -45,21 +32,7 @@ const Navbar = () => {
                   href="#"
                   className="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
                 >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 mr-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
+                  
                   Calendar
                 </a>
 
@@ -67,21 +40,7 @@ const Navbar = () => {
                   href="#"
                   className="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
                 >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 mr-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    />
-                  </svg>
+                  
                   Mail
                 </a>
 
@@ -89,29 +48,20 @@ const Navbar = () => {
                   href="#"
                   className="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
                 >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 mr-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
+                  
                   Kanban
                 </a>
-              </nav>
 
-              <nav className="space-y-2 mt-64">
+                <nav className="mt-80">
+                  <div className="bg-gray-200 rounded-md shadow-lg m-1 p-2">
+                    <p className="font-bold flex"><FaHeadphonesAlt className="mt-1 mr-2" /> Need support?</p>
+                    <p>Get in  touch with our team.</p>
+                    <button className="text-center items-center justify-center w-full bg-white mt-2 py-1 px-6 rounded-md">Contact us</button>
+
+                  </div>
                 <a
                   href="#"
-                  className="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
+                  className=" flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group"
                 >
                   Settings
                 </a>
@@ -121,6 +71,11 @@ const Navbar = () => {
                 >
                   Log Out
                 </a>
+                </nav>
+              </nav>
+
+              <nav className="space-y-2 mt-64">
+                
               </nav>
             </div>
           </div>
